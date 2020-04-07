c5cd1a2fc03b2a619e9bc7ae7ef3f17b5156423f
yusuke ohno <yusuke@ohno.systems>
2020-04-08 01:38:50 +0900

    samplebox

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import * as ReactDOM from 'react-dom'
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Index from './pages/index'
 import Particle from './pages/detail/particle'
+import SampleBox from './pages/detail/samplebox'
 
 ReactDOM.render(
   <HashRouter>
     <Switch>
       <Route exact path="/" component={Index} />
       <Route exact path="/particle" component={Particle} />
+      <Route exact path="/samplebox" component={SampleBox} />
       <Redirect to="/" />
     </Switch>
   </HashRouter>,
