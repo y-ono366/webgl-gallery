279204109499d615e5b8dce24db919dea370899f
yusuke ohno <yusuke@ohno.systems>
2020-04-28 19:43:09 +0900

    update

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -4,32 +4,28 @@ import styled from 'styled-components'
 const Header: React.FC = () => (
   <div>
     <Left>
-      <h1>WEBGL GALLERY</h1>
+      <Logo> webgl / gallery</Logo>
     </Left>
     <Right></Right>
   </div>
 )
 
 const Left = styled.div`
-  position: fixed;
-  left: 50px;
-  top: 30px;
-  z-index: 9;
-
-  h1 {
-    font-size: 32px;
-    line-height: 36px;
-    font-weight: 400;
-    // letter-spacing: 0.3em;
-    text-align: left;
-    color: #c2c2c2;
-  }
+  //z-index: 9;
 `
 const Right = styled.div`
   position: fixed;
   right: 50px;
   top: 30px;
   z-index: 9;
+`
+const Logo = styled.h1`
+  width: 420px;
+  height: 50px;
+  position: fixed;
+  z-index: 9;
+  left: 30px;
+  color: #c1c1c1c1;
 `
 
 export default Header
