25d6f6b01959ef43c252e1fad0269c6117329fd5
yusuke ohno <yusuke@ohno.systems>
2020-10-11 14:03:52 +0900

    アニメーション複数 ハンドリング

--- src/pages/detail/twelve.tsx
+++ src/pages/detail/twelve.tsx
@@ -12,63 +12,61 @@ import {
   Object3D,
   AnimationMixer,
   Clock,
+  Raycaster,
+  AnimationClip,
+  LoopOnce,
 } from 'three'
-import { TweenMax } from 'gsap'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const scene = new Scene()
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(0, 0, 10)
 const light = new DirectionalLight(0xffffff)
+light.position.set(0, 0, 10)
 const canvas = React.createRef<HTMLCanvasElement>()
-const geometry = new BoxGeometry(1, 1, 1)
-const material = new MeshNormalMaterial()
 let animateFrameId = 0
 let model: Object3D = null
-const mixers = []
+let mixers: AnimationMixer = null
+let animations: AnimationClip[] = []
+
+const getMesh = (_o: Object3D) => {
+  let refO = null
+
+  if (_o.type.toLowerCase().indexOf('skinnedmesh') > -1) {
+    return _o
+  }
+
+  for (let i = 0; i < _o.children.length; i++) {
+    refO = getMesh(_o.children[i])
+    if (refO) {
+      break
+    }
+  }
+  return refO
+}
 
 const Twelve: React.FC = () => {
-  const getMesh = (_o: Object3D) => {
-    let refO = null
-
-    if (_o.type.toLowerCase().indexOf('skinnedmesh') > -1) {
-      return _o
-    }
-
-    for (let i = 0; i < _o.children.length; i++) {
-      refO = getMesh(_o.children[i])
-      if (refO) {
-        break
-      }
-    }
-    return refO
-  }
   React.useEffect(() => {
     const renderer = new WebGLRenderer({
       antialias: true,
       canvas: canvas.current,
     })
-    const loader = new GLTFLoader()
-    loader.load(
-      'http://localhost:8080/assets/human_animation.glb',
+
+    new GLTFLoader().load(
+      'http://localhost:8080/assets/human_animation2.glb',
       function (gltf) {
         model = gltf.scene as Object3D
         model.position.y = -3
         scene.add(model)
 
-        const animations = gltf.animations
+        animations = gltf.animations
 
         if (animations && animations.length) {
           const meshes = getMesh(model)
-
-          let mixer = new AnimationMixer(meshes)
-          for (let m = 0; m < animations.length; m++) {
-            mixer.clipAction(animations[m]).play()
-          }
-          mixers.push(mixer)
-          if (mixers) {
-            mixer = mixers[0]
-          }
+          const mixer = new AnimationMixer(meshes)
+          mixers = mixer
         }
+
         animate()
       },
       function (onprogress) {
@@ -81,10 +79,20 @@ const Twelve: React.FC = () => {
       }
     )
 
-    camera.position.set(0, 0, 10)
-    light.position.set(0, 0, 10)
     scene.add(light)
     renderer.setSize(window.innerWidth, window.innerHeight)
+
+    canvas.current.addEventListener('click', (event) => {
+      mixers.stopAllAction()
+      const anim = mixers.clipAction(animations[0])
+      anim.setLoop(LoopOnce, 0).play()
+    })
+
+    canvas.current.addEventListener('dblclick', (event) => {
+      mixers.stopAllAction()
+      const anim = mixers.clipAction(animations[3])
+      anim.setLoop(LoopOnce, 0).play()
+    })
 
     const clock = new Clock()
     const animate = () => {
@@ -92,17 +100,12 @@ const Twelve: React.FC = () => {
 
       model.rotation.y += 0.01
 
-      if (mixers) {
-        for (let i = 0; i < mixers.length; i++) {
-          mixers[i].update(clock.getDelta())
-        }
-      }
+      mixers.update(clock.getDelta())
 
       renderer.render(scene, camera)
     }
     return () => {
-      material.dispose()
-      geometry.dispose()
+      cancelAnimationFrame(animateFrameId)
       scene.remove(model)
       scene.remove(light)
     }
